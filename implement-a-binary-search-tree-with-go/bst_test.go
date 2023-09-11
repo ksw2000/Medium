3ec4245ac246f5866d12e268c65c1ce3fe3fb3f6
Kashiwa <liao2000@livemail.tw>
2023-09-12 02:43:16 +0800

    Add 2 alternate insertion and unit test function (cont.)

--- implement-a-binary-search-tree-with-go/bst_test.go
+++ implement-a-binary-search-tree-with-go/bst_test.go
@@ -2,6 +2,7 @@ package bst
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 )
 
@@ -51,5 +52,108 @@ func TestBST(t *testing.T) {
 	// Search by key: 152
 	if n, exist := tree.Search(152); exist {
 		fmt.Println(152, n.Val)
+	}
+}
+
+// go test -run TestBSTCorrectness
+func TestBSTCorrectness(t *testing.T) {
+	// generate an int list
+	data := generateData(1024)
+	// create an empty binary search tree
+	tree := new(BST)
+	// insert data
+	for i := range data {
+		tree.Insert(&BSTNode{Key: data[i]})
+	}
+
+	// check the binary search tree is in increasing order
+	// and the total number of nodes is equal to the length of `data`
+	i := 0
+	start := 0
+	tree.Do(func(b *BSTNode) {
+		if b.Key >= start {
+			start = b.Key
+		} else {
+			t.Fail()
+		}
+		i++
+	})
+	if i != len(data) {
+		t.Fail()
+	}
+
+	// check that we can access all element by tree.Search()
+	for i := range data {
+		_, found := tree.Search(data[i])
+		if !found {
+			t.Fail()
+		}
+	}
+
+	// check that we can remove each nodes
+	k := len(data)
+	for i := range data {
+		tree.Delete(data[i])
+		k--
+		// check that the tree is in increasing order
+		// and the total number of nodes is equal properly
+		if i%32 == 0 {
+			n := 0
+			start := 0
+			tree.Do(func(b *BSTNode) {
+				if b.Key >= start {
+					start = b.Key
+				} else {
+					t.Fail()
+				}
+				n++
+			})
+			if n != k {
+				t.Fail()
+			}
+		}
+	}
+}
+
+func generateData(size int) []int {
+	rand.NewSource(0)
+	list := make([]int, size)
+	for i := range list {
+		list[i] = i
+	}
+	rand.Shuffle(len(list), func(i, j int) {
+		list[i], list[j] = list[j], list[i]
+	})
+	return list
+}
+
+// go test -bench=BenchmarkInsert -run=none
+func BenchmarkInsertIterative(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		list := generateData(4096)
+		tree := new(BST)
+		for i := range list {
+			tree.Insert(&BSTNode{Key: list[i]})
+		}
+	}
+}
+
+func BenchmarkInsertIterative2(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		list := generateData(4096)
+		tree := new(BST)
+		for i := range list {
+			tree.Insert2(&BSTNode{Key: list[i]})
+		}
+	}
+}
+
+func BenchmarkInsertRecursive(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		list := generateData(4096)
+		tree := new(BST)
+		for i := range list {
+			tree.InsertRec(&BSTNode{Key: list[i]})
+		}
 	}
 }
